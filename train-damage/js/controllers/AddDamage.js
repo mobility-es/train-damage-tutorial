4e62e37925dd4c8ae90fa68b36f13cee1d14eb5a
Christophe Bram <cbr@appearnetworks.com>
2013-05-21 11:56:37 +0200

    Initial version

--- train-damage/js/controllers/AddDamage.js
+++ train-damage/js/controllers/AddDamage.js
@@ -1,5 +1,6 @@
 AIQ.Spine.Controller.sub({
-    init: function () {
+    events: {
+        "click button": "_onSaveClicked"
     },
 
     render: function (params) {
@@ -13,5 +14,22 @@ AIQ.Spine.Controller.sub({
 
         // If we don't navigate to another page, we should always end this render function with "return this"
         return this;
+    },
+
+    _onSaveClicked: function(e) {
+        var newDamage = new TD.Damage({
+            trainId: this.trainId,
+            description: this.$("textarea").val()
+        });
+
+        // Persisting the new defect in the Data-sync layer. Since it's a new defect, "save()" will create it.
+        // Had it been an existing defect, "save()" would have updated it.
+        newDamage.save();
+
+        // For debugging purposes only, we check that newDamage comes out when calling all()
+        console.log(TD.Damage.all());
+
+        // Synchronizing with the platform
+        AIQ.Core.DataSync.synchronize();
     }
 }).registerAs("/add-damage/:trainId", "AddDamage.tmpl");
